c19f969bff7830cdb51e1b139d9e0b67f60e009c
Doson <karbushev-1991@mail.ru>
2023-01-07 21:42:30 +0700

    import rename

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as ex
+from rename_file import rename_file
 
 
 def save_file(driver):
@@ -14,11 +15,12 @@ def save_file(driver):
     :param driver:
     :return:
     """
-    WebDriverWait(driver, timeout=10).until(ex.presence_of_element_located((By.CLASS_NAME, '_93444fe79c--main--PpO9F._93444fe79c--light--fV8h9')))
+    WebDriverWait(driver, timeout=10).until(
+        ex.presence_of_element_located((By.CLASS_NAME, '_93444fe79c--main--PpO9F._93444fe79c--light--fV8h9')))
     button_download = driver.find_element(By.CLASS_NAME, '_93444fe79c--main--PpO9F._93444fe79c--light--fV8h9')
     button_download.send_keys(Keys.ENTER)
     WebDriverWait(driver, timeout=10).until(ex.presence_of_element_located((By.CLASS_NAME,
-                                       '_93444fe79c--button--Cp1dl._93444fe79c--button--IqIpq._93444fe79c--M--T3GjF._93444fe79c--button--jVM22')))
+                                                                            '_93444fe79c--button--Cp1dl._93444fe79c--button--IqIpq._93444fe79c--M--T3GjF._93444fe79c--button--jVM22')))
     button_save = driver.find_elements(By.CLASS_NAME,
                                        '_93444fe79c--button--Cp1dl._93444fe79c--button--IqIpq._93444fe79c--M--T3GjF._93444fe79c--button--jVM22')
     if len(button_save) == 1:
@@ -35,11 +37,11 @@ def authorization(driver):
     :return:
     """
     WebDriverWait(driver, timeout=10).until(ex.presence_of_element_located((By.CLASS_NAME,
-                                             '_25d45facb5--button--Cp1dl._25d45facb5--button--IqIpq._25d45facb5--M--T3GjF.'
-                                             '_25d45facb5--button--jfWOF._25d45facb5--full-width--MF714')))
+                                        '_25d45facb5--button--Cp1dl._25d45facb5--button--IqIpq._25d45facb5--M--T3GjF.'
+                                        '_25d45facb5--button--jfWOF._25d45facb5--full-width--MF714')))
     authorization_type = driver.find_element(By.CLASS_NAME,
-                                             '_25d45facb5--button--Cp1dl._25d45facb5--button--IqIpq._25d45facb5--M--T3GjF.'
-                                             '_25d45facb5--button--jfWOF._25d45facb5--full-width--MF714')
+                                        '_25d45facb5--button--Cp1dl._25d45facb5--button--IqIpq._25d45facb5--M--T3GjF.'
+                                         '_25d45facb5--button--jfWOF._25d45facb5--full-width--MF714')
     authorization_type.send_keys((Keys.ENTER))
 
     WebDriverWait(driver, timeout=10).until(ex.presence_of_element_located((By.NAME, 'username')))
@@ -47,8 +49,8 @@ def authorization(driver):
     email.send_keys('karbushev-1991@mail.ru')
 
     WebDriverWait(driver, timeout=10).until(ex.presence_of_element_located((By.CLASS_NAME,
-                                    '_25d45facb5--button--Cp1dl._25d45facb5--button--IqIpq._25d45facb5--M--T3GjF.'
-                                    '_25d45facb5--button--OhHnj._25d45facb5--full-width--MF714')))
+                                        '_25d45facb5--button--Cp1dl._25d45facb5--button--IqIpq._25d45facb5--M--T3GjF.'
+                                        '_25d45facb5--button--OhHnj._25d45facb5--full-width--MF714')))
     continuee = driver.find_element(By.CLASS_NAME,
                                     '_25d45facb5--button--Cp1dl._25d45facb5--button--IqIpq._25d45facb5--M--T3GjF.'
                                     '_25d45facb5--button--OhHnj._25d45facb5--full-width--MF714')
@@ -64,6 +66,23 @@ def authorization(driver):
 
 def main():
     driver = init_webdriver()
+    # 200 обьявлений, не включена лоджия
+    url1 = 'https://krasnoyarsk.cian.ru/cat.php?deal_type=sale&' \
+           'engine_version=2&' \
+           'floornl=1&' \
+           'house_material%5B0%5D=1&' \
+           'house_material%5B1%5D=2&' \
+           'house_material%5B2%5D=4&' \
+           'house_material%5B3%5D=8&' \
+           'is_first_floor=0&' \
+           'min_balconies=1&' \
+           'minsu_r=1&' \
+           'offer_type=flat&' \
+           'region=4827&' \
+           'room1=1&' \
+           'room2=1&' \
+           'room3=1'
+    # 600+ c лоджией и балконом, новостройки и вторичка
     url = 'https://krasnoyarsk.cian.ru/cat.php?deal_type=sale&' \
           'engine_version=2&' \
           'floornl=1&' \
@@ -72,13 +91,12 @@ def main():
           'house_material%5B2%5D=4&' \
           'house_material%5B3%5D=8&' \
           'is_first_floor=0&' \
+          'loggia=1&' \
           'min_balconies=1&' \
           'minsu_r=1&' \
           'offer_type=flat&' \
           'region=4827&' \
-          'room1=1&' \
-          'room2=1&' \
-          'room3=1'
+          'room1=1&room2=1&room3=1'
 
     driver.get(url)
     driver.set_window_size(1920, 1080)
@@ -89,21 +107,23 @@ def main():
     time.sleep(10)
     try:
         error = driver.find_element(By.CLASS_NAME, '_93444fe79c--color_white_100--YUO3d.'
-                                               '_93444fe79c--lineHeight_5u--cJ35s.'
-                                               '_93444fe79c--fontWeight_normal--P9Ylg.'
-                                               '_93444fe79c--fontSize_14px--TCfeJ.'
-                                               '_93444fe79c--display_block--pDAEx.'
-                                               '_93444fe79c--text--g9xAG.'
-                                               '_93444fe79c--text_letterSpacing__0--mdnqq')
+                                                   '_93444fe79c--lineHeight_5u--cJ35s.'
+                                                   '_93444fe79c--fontWeight_normal--P9Ylg.'
+                                                   '_93444fe79c--fontSize_14px--TCfeJ.'
+                                                   '_93444fe79c--display_block--pDAEx.'
+                                                   '_93444fe79c--text--g9xAG.'
+                                                   '_93444fe79c--text_letterSpacing__0--mdnqq')
     except:
         error = None
+        print('[+] Download Complete')
+        driver.quit()
 
     if error:
         print('[-] DownloadError: Превышен лимит скачиваний для некоммерческого использования, попробуйте завтра')
         time.sleep(120)
-    print('[+] Download Complete')
-    driver.quit()
+        driver.quit()
 
 
 if __name__ == '__main__':
     main()
+    rename_file('offers.xlsx')
